554d1ba3b8c64892b14a5157fc5f256fa2473f5c
Sristi Shreya <shreyasristi@gmail.com>
2023-09-30 01:57:40 +0530

    Controller-Model-FileStorage

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const productController = require('../controller/products')
+
+const router = express.Router();
+
+
+// /admin/add-product => GET
+router.get('/add-product', productController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productController.postProducts);
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+const productController = require('../controller/products');
+
+router.get('/', productController.getProducts);
+
+module.exports = router;
